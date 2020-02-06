e08ef53b2f0a1f00fc93e4cc20173b674fe142dc
crispthoalex <alex877@gmail.com>
2020-02-06 01:40:14 +0000

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print string of the project
+ * Return:0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
