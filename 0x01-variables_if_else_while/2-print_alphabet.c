127e1adb182df5fae9ecb52d29bc379233437017
crispthoalex <alex877@gmail.com>
2020-02-06 21:27:30 +0000

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - print random number and say positve, negative or zero
+ * Return:0
+ */
+int main(void)
+{
+char az;
+for (az = 'a'; az <= 'z'; az++)
+putchar(az);
+putchar('\n');
+return (0);
+}
