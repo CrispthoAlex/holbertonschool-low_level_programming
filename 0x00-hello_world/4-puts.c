72b65ef5170468587574c915d7ba935c3c0b8f4f
crispthoalex <alex877@gmail.com>
2020-02-06 01:25:15 +0000

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**Hello Project - Will be compile to create a executable file
+* main - Prints the string Programming is like building...
+* Return (0)
+*/
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
