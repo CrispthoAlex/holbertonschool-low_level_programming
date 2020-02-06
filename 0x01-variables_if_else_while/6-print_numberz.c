edb695b8b839df48020224524237fc0e2e2b4001
crispthoalex <alex877@gmail.com>
2020-02-06 23:07:36 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - print alphAbet
+ * Return:0
+ */
+int main(void)
+{
+int n;
+for (n = 48 ; n <= 57 ; n++) /* print 0-9 with ASCII code*/
+putchar(n);
+putchar('\n');
+return (0);
+}
