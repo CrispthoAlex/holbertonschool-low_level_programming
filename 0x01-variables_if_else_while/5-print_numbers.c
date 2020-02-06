e1db2e4ed6f7e42dae25d2adcb02a7fc2367912b
crispthoalex <alex877@gmail.com>
2020-02-06 22:53:29 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - print alphAbet
+ * Return:0
+ */
+int main(void)
+{
+int n;
+for (n = '0' ; n <= '9' ; n++) /* print 0-9 */
+putchar(n);
+putchar('\n');
+return (0);
+}
