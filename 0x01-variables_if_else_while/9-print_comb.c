f88c94dcb7c98dd66327af452b5d66664f455b2a
crispthoalex <alex877@gmail.com>
2020-02-07 01:04:51 +0000

    Patience, persistence and perspiration

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - print alphAbet
+ * Return:0
+ */
+int main(void)
+{
+int n;
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+if (n != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
