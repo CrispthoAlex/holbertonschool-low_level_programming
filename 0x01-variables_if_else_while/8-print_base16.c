6b0579ed213cc8d76d722f2547e92825abc73e71
crispthoalex <alex877@gmail.com>
2020-02-07 00:11:51 +0000

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - print alphAbet
+ * Return:0
+ */
+int main(void)
+{
+int n0;
+int n1;
+for (n0 = 48 ; n0 <= 57; n0++)
+putchar(n0);
+for (n1 = 97; n1 <= 102; n1++)
+putchar(n1);
+putchar('\n');
+return (0);
+}
