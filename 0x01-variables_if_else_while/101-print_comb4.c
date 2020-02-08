05994b4d29933757f33349cd6ce4e10c6ecf1bc6
crispthoalex <alex877@gmail.com>
2020-02-08 02:14:54 +0000

    The success combination in business is: Do what you do better

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Combinations
+ * Return:0
+ */
+int main(void)
+{
+int i;
+int n;
+int m; /* unidades, decenas, centenas */
+
+for (i = 48; i <= 55; i++) /* First Loop*/
+{
+  for (n = i + 1; n <= 56; n++) /* Second Loop*/
+    {
+      for (m = 50; m <= 57; m++) /* Thirth Loop*/
+	{
+	  if ( (m != n) && (n > i) )
+	      putchar(i);/* centenas  */
+	      putchar(n);/* decenas  */
+	      putchar(m);/* decenas */
+	      if ( (i < 55) || ( (n < 56)  || (m < 57) ) ) /* */
+		{
+		  putchar(',');
+		  putchar(' ');
+		}
+	}
+    }
+}
+putchar('\n');
+return (0);
+}
