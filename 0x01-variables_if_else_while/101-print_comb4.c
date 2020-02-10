3790e5dd065433a7f4cfcb1556798cc39e5dfa57
crispthoalex <alex877@gmail.com>
2020-02-10 15:00:48 +0000

    Do what you do better... and: do more of what you do

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,21 +13,21 @@ int m; /* unidades, decenas, centenas */
 
 for (i = 48; i <= 55; i++) /* First Loop*/
 {
-  for (n = i + 1; n <= 56; n++) /* Second Loop*/
-    {
-      for (m = 50; m <= 57; m++) /* Thirth Loop*/
-	{
-	  if ( (m != n) && (n > i) )
-	      putchar(i);/* centenas  */
-	      putchar(n);/* decenas  */
-	      putchar(m);/* decenas */
-	      if ( (i < 55) || ( (n < 56)  || (m < 57) ) ) /* */
-		{
-		  putchar(',');
-		  putchar(' ');
-		}
-	}
-    }
+for (n = i + 1; n <= 56; n++) /* Second Loop*/
+{
+for (m = n + 1; m <= 57; m++) /* Thirth Loop*/
+{
+if ((m != n) && (n > i))
+putchar(i);/* centenas  */
+putchar(n);/* decenas  */
+putchar(m);/* unidades */
+if ((i < 55) || ((n < 56)  || (m < 57))) /* */
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
 }
 putchar('\n');
 return (0);
