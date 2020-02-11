a5f67312e45cfcd64643bf741d4ff6b81c2e7d3d
crispthoalex <alex877@gmail.com>
2020-02-11 14:19:36 +0000

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
  * _islower - checks is lower
- *
+ * @c - type char, from 3-main.c | checks is lower
  * Return: always 0.
  */
-int _islower(int c) /* c - type char, from 3-main.c | checks is lower */
+int _islower(int c)
 {
 	if ((c >= 97) && (c <= 122))
 	return (1);
