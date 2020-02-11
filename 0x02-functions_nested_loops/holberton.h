3b626e7c90b0edc6e281c1b87c14853bbd29661f
crispthoalex <alex877@gmail.com>
2020-02-11 23:51:02 +0000

    header file

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -8,4 +8,5 @@ int print_sign(int n); /* task 05 sig of number */
 int _abs(int i); /* task 06 absolute value of an integer */
 int print_last_digit(int n); /* 07 - last digit */
 void jack_bauer(void); /* every minute of the day of Jack Bauer  */
-void times_table(void) /* prints the 9 times table, starting with 0 */
+void times_table(void); /* prints the 9 times table, starting with 0 */
+int add(int i, int j); /* adds two integers and returns the result */
