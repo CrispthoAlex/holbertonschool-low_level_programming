6419dcf31086ee12bcc10401086f7296f1f9468e
crispthoalex <alex877@gmail.com>
2020-02-11 05:06:40 +0000

    header file

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,7 @@
+/** function _putchar */
+int _putchar(char c);
+void print_alphabet(void); /* task 01*/
+void print_alphabet_x10(void); /* task 02*/
+int _islower(int c); /* task 03 checks is lower */
+int _isalpha(int c); /* task 04 isalpha  */
+int print_sign(int n); /* task 05 sig of number */
