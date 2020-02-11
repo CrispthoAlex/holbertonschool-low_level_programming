20d3f15241eb2066ee023e9ddc65fa63d18e4a1a
crispthoalex <alex877@gmail.com>
2020-02-11 15:24:51 +0000

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,7 +12,7 @@ void print_alphabet_x10(void) /* print alphabet X10 - Rename this function */
 
 	for (j = 0; j <= 9; j++)
 	{
-	for (i = 0; i <= 26; i++)
+	for (i = 0; ho[i] != '\0'; i++)
 	{
 		_putchar(ho[i]);
 	}
