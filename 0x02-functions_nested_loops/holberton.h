dd82a588c499181aaf45bf9be043ac7ccb05e098
crispthoalex <alex877@gmail.com>
2020-02-11 19:58:34 +0000

    header file

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -6,3 +6,4 @@ int _islower(int c); /* task 03 checks is lower */
 int _isalpha(int c); /* task 04 isalpha  */
 int print_sign(int n); /* task 05 sig of number */
 int _abs(int i); /* task 06 absolute value of an integer */
+int print_last_digit(int n); /* 07 - last digit */
