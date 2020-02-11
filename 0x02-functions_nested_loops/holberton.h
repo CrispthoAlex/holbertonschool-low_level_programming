d994faa02c49f24665bb19e3343072a4b1cb546b
crispthoalex <alex877@gmail.com>
2020-02-11 20:07:30 +0000

    header file

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -7,3 +7,4 @@ int _isalpha(int c); /* task 04 isalpha  */
 int print_sign(int n); /* task 05 sig of number */
 int _abs(int i); /* task 06 absolute value of an integer */
 int print_last_digit(int n); /* 07 - last digit */
+void jack_bauer(void); /* every minute of the day of Jack Bauer  */
