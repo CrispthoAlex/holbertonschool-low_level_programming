78c06ac1fac21b4fd87687abba45ce4704f6240a
crispthoalex <alex877@gmail.com>
2020-02-12 05:14:00 +0000

    Holberton School, 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,6 @@ void print_to_98(int n)
 		for (; n <= 98; n++)
 		{
 			printf("%d", n);
-
 			if (n != 98)
 			{
 				putchar(',');
@@ -26,7 +25,6 @@ void print_to_98(int n)
 		for (; n >= 98; n--)
 		{
 			printf("%d", n);
-
 			if (n != 98)
 			{
 				putchar(',');
