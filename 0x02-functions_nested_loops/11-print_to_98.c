e185521ddf2d050e2071f75e8324915e462fdff0
crispthoalex <alex877@gmail.com>
2020-02-12 04:40:02 +0000

    Holberton School, 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - natural number
+ * @n: enter first number
+ * Return: result
+ */
+void print_to_98(int n)
+{
+	int i = 98;
+
+	if (n <= i)
+	{
+		for ( ; n <= i; n++)
+		{
+			printf("%d, ", n);
+
+			if (n != i)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for ( ; n >= i; n--)
+		{
+			printf("%d, ", n);
+
+			if (n != i)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+}
