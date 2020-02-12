4ac24b99b5952d410fffaf1d27a28bfb82b9af39
crispthoalex <alex877@gmail.com>
2020-02-12 02:15:10 +0000

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,12 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
-	while (i < 10)
-	{
-		putchar(i);
-	} /* variable 'a' will increase ( a++ ) */
-
+	/**
+	 *while (i < 10)
+	 *{
+	 *	putchar(i);
+	 *}  variable 'a' will increase ( a++ )
+	 */
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
