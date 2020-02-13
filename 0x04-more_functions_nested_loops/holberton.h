740dcd78c4fd1659e7a492b3c43ace6ce4db3213
crispthoalex <alex877@gmail.com>
2020-02-13 16:18:41 +0000

    More functions header file

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -2,7 +2,11 @@
 #define HOLBERTON_H
 
 #include <stdio.h>
-
+int _putchar(char c);
+/* will include _putchar */
 int _isupper(int c); /* 0 is supper  */
 int _isdigit(int c); /* 1 is digit */
+int mul(int a, int b); /* 2 multiplication  */
+void print_numbers(void); /* 3 numbers speak, 0-9 */
+void print_most_numbers(void); /* 4 more numbers, not 2 and 4 */
 #endif /* HOLBERTON_H */
