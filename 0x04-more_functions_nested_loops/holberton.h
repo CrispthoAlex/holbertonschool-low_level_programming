d3cf88b6cf1f3d6ad76f084ad9443874c060f4aa
crispthoalex <alex877@gmail.com>
2020-02-13 17:51:19 +0000

    More functions header file

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -10,4 +10,5 @@ int mul(int a, int b); /* 2 multiplication  */
 void print_numbers(void); /* 3 numbers speak, 0-9 */
 void print_most_numbers(void); /* 4 more numbers, not 2 and 4 */
 void more_numbers(void); /*5. Numbers universal language */
+void print_line(int n); /*6 print line */
 #endif /* HOLBERTON_H */
