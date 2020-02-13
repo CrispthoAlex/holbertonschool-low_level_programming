00bdd7996cfff581bfa15949c1bd6480e23b4662
crispthoalex <alex877@gmail.com>
2020-02-13 02:52:54 +0000

    header file

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -11,4 +11,4 @@ void jack_bauer(void); /* task 8 every minute of the day of Jack Bauer  */
 void times_table(void); /* task 9 prints the 9 times table, starting with 0 */
 int add(int i, int j); /* task 10 adds two integers and returns the result */
 void print_to_98(int n); /* task 11 prints all natural numbers from n to 98 */
-void print_times_table(int n) /* print n times table */
+void print_times_table(int n); /* task 12 print n times table */
