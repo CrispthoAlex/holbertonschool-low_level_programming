195a5b702b03544d0138cb5312800f0b11880a77
crispthoalex <alex877@gmail.com>
2020-02-13 17:08:30 +0000

    5. Numbers constitute the only universal language

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - check the code for Holberton School students.
+ *
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j < 10)
+			{
+				_putchar (j % 10 + '0');
+			} else
+			{
+				_putchar (j / 10 + '0'); /* first digit*/
+			_putchar (j % 10 + '0'); /* second digit */
+			}
+}
+		_putchar ('\n');
+	}
+}
