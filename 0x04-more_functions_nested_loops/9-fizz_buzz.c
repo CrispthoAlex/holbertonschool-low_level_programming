980838b72821c4bcc4b157845e3b2eb397d22b77
crispthoalex <alex877@gmail.com>
2020-02-13 22:56:15 +0000

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * fizz-buzz - check the code for Holberton School students.
+ *
+ *
+ * Return: Always 0.
+ */
+int main (void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 1; i <= 100; i++)
+	{
+		j = i % 3; /* multiplos de 3*/
+		k = i % 5; /* multiplos de 5*/
+
+		if ((j == 0) && (k == 0)) /* print 3X or 5X */
+		{
+			printf ("FIZZBUZZ ");
+		}
+		else if (j == 0)
+		{
+			printf ("FIZZ ");
+		}
+		else if (k == 0)
+		{
+			printf ("BUZZ ");
+		}
+		else
+			printf ("%d ", i);
+	}
+	putchar ('\n');
+return (0);
+}
