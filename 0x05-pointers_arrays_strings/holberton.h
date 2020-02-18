4893b0cf481b482015b44c6e0d34a57b439e7c8f
crispthoalex <alex877@gmail.com>
2020-02-18 22:08:55 +0000

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -7,4 +7,5 @@ int _putchar(char c);
 void reset_to_98(int *n); /* 0 - reset to 98  */
 void swap_int(int *a, int *b); /* 1 - swaps the values of two integers */
 int _strlen(char *s); /* 2 - returns the length of a string */
+void _puts(char *str); /* prints a string, followed by a new line */
 #endif /* HOLBERTON_H */
