edfc58f129e2584662e43945e999920ee6de85b3
crispthoalex <alex877@gmail.com>
2020-02-19 04:23:31 +0000

    7. Winning is only half of it

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,13 +17,13 @@ void puts_half(char *str)
 	{
 		i++; /* count */
 	}
-	if (i % 2)
+	if (i % 2 == 0)
 	{
 		j = (i / 2);
 	}
 	else
 	{
-		j = (i - 1) / 2; /* Decide what j = n to use  */
+		j = (i - 1) / 2 + 1; /* Decide what j = n to use  */
 	}
 	while (j < i)
 	{
