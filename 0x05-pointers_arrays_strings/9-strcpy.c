b53378e0956fc8e89a6c34b1a65cb75e27556751
crispthoalex <alex877@gmail.com>
2020-02-19 05:32:57 +0000

    9. strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - returns string
+ * @dest: array string
+ * @src: n numbres to print
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+
+}
