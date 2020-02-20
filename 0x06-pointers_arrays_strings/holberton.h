5e8a6412f6cf2ef6c1eb7e90591d9cfd60f5eb17
crispthoalex <alex877@gmail.com>
2020-02-20 16:02:00 +0000

    0x06. C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+int _putchar(char c);
+/* will include _putchar */
+char *_strcpy(char *dest, char *src); /* project 0x05 - 9 - copy string  */
+char *_strcat(char *dest, char *src); /* 0 - concatenates string */
+#endif /* HOLBERTON_H */
