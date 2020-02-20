e9f7c4b6da144b5f129060b07a440fc69ff2fdf0
crispthoalex <alex877@gmail.com>
2020-02-20 19:19:31 +0000

    concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *_strcat - returns concatenate string
+ * @dest: array 0 Hello string
+ * @src: array 1 World string
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j; /* length dest */
+
+	/*dest = "Hello ";
+	  src = "World!\n";*/
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = i; /* length dest without '\0' */
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[j] = src[i];/* concatenat */
+		j++;
+	}
+
+	return (dest);
+}
