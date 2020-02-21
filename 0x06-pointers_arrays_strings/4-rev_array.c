fa58d6da1db8c7670adbc2715c644c67f8276ce6
crispthoalex <alex877@gmail.com>
2020-02-21 02:34:06 +0000

    reverses the content of an array of integers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - reverses array integers
+ * @a: array to reverse
+ * @n: size to reverse
+ *
+ * Return: Always 0.
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+	int revs;
+
+	i = 0; /* first count */
+	j = n - 1;
+	while (i < j)
+	{
+		revs = a[i];
+		a[i] = a[j];
+		a[j] = revs;
+		i++;
+		j--;
+	}
+}
