fe23ba925824bc176b842963224288ec7afc7869
crispthoalex <alex877@gmail.com>
2020-02-21 01:37:42 +0000

    compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,11 +13,15 @@ int _strcmp(char *s1, char *s2)
 	int i;
 	int cmp; /* value compares */
 
-	i = 0;
 	cmp = 0;
-	if (s1[i] != s2[i] && (s1[i] != '\0' && s2[i] != '\0'))
+	i = 0;
+	while ((cmp == 0) && (s1[i] != '\0' || s2[i] != '\0'))
 	{
-		cmp = s1[i] - s2[i];
+		if (s1[i] != s2[i])
+		{
+			cmp = s1[i] - s2[i];
+		}
+		i++;
 	}
 
 return (cmp);
