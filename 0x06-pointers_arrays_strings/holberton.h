d3c6377c342f883ee3529ed111dfc195465a5239
crispthoalex <alex877@gmail.com>
2020-02-21 03:19:21 +0000

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -10,4 +10,5 @@ char *_strncat(char *dest, char *src, int n);/*1-concatenates string with n*/
 char *_strncpy(char *dest, char *src, int n); /*2- copy string from n */
 int _strcmp(char *s1, char *s2); /* 3- compares string */
 void reverse_array(int *a, int n); /*reverses contents of an array of integers*/
+char *string_toupper(char *); /* lowercase to uppercase*/
 #endif /* HOLBERTON_H */
