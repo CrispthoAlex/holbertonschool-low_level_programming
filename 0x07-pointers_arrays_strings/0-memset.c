5e7ca2fec92a0641c8665334755213809b7bc81e
crispthoalex <alex877@gmail.com>
2020-02-24 17:29:11 +0000

    Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *_memset - prints buffer in hexa
+ * @s: the memory area pointed to by s
+ * @b: the constant byte c
+ * @n: the first n bytes
+ * Return: type char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (0);
+
+}
---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+int _putchar(char c);
+/* will include _putchar */
+char *_memset(char *s, char b, unsigned int n);/*0fillsmemorywithaconstantbyte*/
+char *_memcpy(char *dest, char *src, unsigned int n);/*1-copies memory area*/
+char *_strchr(char *s, char c);/*2-locates a character in a string*/
+unsigned int _strspn(char *s, char *accept);/*3-getslength of aprefixsubstring.*/
+#endif /* HOLBERTON_H */
