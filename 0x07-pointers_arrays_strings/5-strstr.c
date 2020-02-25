e83dc2a49b443336b60afd8fb1f40bc8821dc486
crispthoalex <alex877@gmail.com>
2020-02-25 04:12:47 +0000

    locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strstr - locates a character in a string
+ * @haystack: string base
+ * @needle: string to compare
+ *
+ * Return: haystack in n bytes
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i, j;
+	int n; /* count */
+
+	for (i = 0; haystack[i] != '\0'; haystack++)
+	{
+		n = i;
+		for (j = 0; haystack[i] == needle[j] && haystack[i] != '\0'
+			     && needle[j] != '\0'; i++, j++)
+		{
+			return (haystack += n);
+		}
+	}
+	haystack = 0;
+	return (haystack);
+} /* function end */
