b77765de1be527902bd77c483b3cc030eabce73c
crispthoalex <alex877@gmail.com>
2020-02-25 04:59:01 +0000

    0x07. C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -9,5 +9,9 @@ char *_memcpy(char *dest, char *src, unsigned int n);/*1-copies memory area*/
 char *_strchr(char *s, char c);/*2-locates a character in a string*/
 unsigned int _strspn(char *s, char *accept);/*3-getslength of aprefixsubstring.*/
 char *_strpbrk(char *s, char *accept);/*4-searches astringforanyof a setofbytes*/
-char *_strstr(char *haystack, char *needle);/* locates a substring */
+char *_strstr(char *haystack, char *needle);/*5-locates a substring */
+void print_chessboard(char (*a)[8]); /* 6 prints the chessboard */
+void print_diagsums(int *a, int size); /*7-sum of the two diagonals of a
+square matrix of integers*/
+void set_string(char **s, char *to);/*8-sets the value of a pointer to a char */
 #endif /* HOLBERTON_H */
