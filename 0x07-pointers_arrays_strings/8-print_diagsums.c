2bff3129fcd189030290334935289109ed63eee2
crispthoalex <alex877@gmail.com>
2020-02-26 01:16:10 +0000

    prints the sum of the two diagonals

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,20 +11,18 @@
 void print_diagsums(int *a, int size)
 {
 	int i, j;
-	long int addi, addj = 0; /*var addition */
+	int addi, addj = 0; /*var addition */
 
 	i = 0;
 	j = 0;
-	while (i < (size * size))
+	for (; i < (size * size); i += size + 1)
 	{
 		addi += a[i]; /* addition position i*/
-		i += size + 1;
 	}
 	j = size - 1;
-	while (j < (size * size) - 1 && j >= 0)
+	for (; j < ((size * size) - 1) && j >= 0; j += size - 1)
 	{
 		addj += a[j]; /* addition position j*/
-		j += size - 1;
 	}
-	printf("%li, %li\n", addi, addj);
+	printf("%d, %d\n", addi, addj);
 }
