4d53e746b2035e70c33044f09309869f51bae457
crispthoalex <alex877@gmail.com>
2020-02-27 05:11:44 +0000

    if the input integer is a prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _primex - return 1 prime 0 else
+ * @n: number to verified if prime
+ * @x: proub % and equal n
+ * Return: natural square root x
+ */
+int _primex(int n, int x)
+{
+	if (n == x) /*if itself */
+		return (1);
+	if (n % x == 0)/* verified multiplo */
+		return (0);
+	return (_primex(n, x + 1));
+}
+
+/**
+ * is_prime_number - if the input integer is a prime number
+ * @n: number to be verified prime
+ *
+ * Return: if it's prime 1 else 0
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1) /* if 0 or 1 */
+		return (0);
+	return (_primex(n, 2));/*inicialized _primex*/
+}
