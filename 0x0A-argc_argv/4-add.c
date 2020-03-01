2c589af47eb891e4b45c94492c34f5280f1b3d66
crispthoalex <alex877@gmail.com>
2020-03-01 08:54:15 +0000

    adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -33,20 +33,21 @@ int main(int argc, char *argv[])
 	int i;
 	int add = 0;
 
-	for (i = 1; i < argc; i++)
+	if (agrc > 1)
 	{
-		if (!(atoi(argv[i]))) /* contain symbols? */
+		for (i = 1; i < argc; i++)
 		{
-			printf("Error\n");
-			return (1);
+			if (_isdigit(argv[i])) /* is digit? */
+			{
+				add += atoi(argv[i]); /*sum position integer*/
+			}
+			else  /* contain symbols? */
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		else if (!(_isdigit(argv[i]))) /* is digit? */
-		{
-			printf("0\n");
-			return (0);
-		}
-		add += atoi(argv[i]); /*sum position integer*/
+		printf("%d\n", add);
 	}
-	printf("%d\n", add);
 	return (0);
 }
