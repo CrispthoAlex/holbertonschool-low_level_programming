e3e13adaa2c3ee70581c2d272f1da52b68bc85d4
crispthoalex <alex877@gmail.com>
2020-03-01 02:22:42 +0000

    prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - return program name argv[0] = **argv
+ * @argv: The character to print
+ * @argc: size array / number of arguments
+ * Return: On success 1 else 0
+ *
+ */
+int main(int argc, char **argv)
+{
+	(void) argv;
+	printf("%d\n", argc); /* print size array / number of arguments */
+
+return (0);
+
+}
