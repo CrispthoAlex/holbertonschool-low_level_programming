c159e3e8e3f3d2a7a0fea54d3999fb3788757df3
crispthoalex <alex877@gmail.com>
2020-03-04 02:58:02 +0000

    2.He who is not courageous enough to take risks

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *str_concat - concats two strings to pointer
+ *
+ * @s1: string 1
+ * @s2: string 2
+ * Return: Nothing.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i;
+	/*int j;*/
+	int size1, size2, size3; /*_strlen() - length string str */
+	char *strconcat;
+
+	if (s1 == '\0')/* if size > 0 str ok */
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	size1 = i;/*size s1 */
+
+	if (s2 == '\0')/* if size2 > 0 s2 ok */
+		return (NULL);
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		i++;
+	}
+	size2 = i + 1;/* size s2, include '\0' */
+
+	strconcat = malloc((size1 + size2) * sizeof(char));/*size2include'\0'*/
+	if (strconcat == NULL) /* if strconcat NULL*/
+		return (NULL);
+	for (i = 0; i < size1; i++)/* reinicialized i*/
+		strconcat[i] = s1[i];
+	for (i = 0; i < size2; i++)/* j init in size1 */
+		strconcat[size1 + i] = s2[i];
+
+	size3 = size1 + size2;
+	strconcat[size3 + 1] = '\0';
+	return (strconcat);
+}
