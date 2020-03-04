34e8c75597d02d2b21b0221082eed27b0a89674e
crispthoalex <alex877@gmail.com>
2020-03-04 03:56:07 +0000

    2.He who is not courageous enough to take risks

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,7 +13,7 @@ char *str_concat(char *s1, char *s2)
 {
 	int i;
 	int j;
-	int size1, size2;/*size3; /_strlen() - length string str */
+	int size1 = 0, size2 = 0, size3 = 0; /*_strlen() - length string str */
 	char *strconcat;
 
 	if (s1)/* if size > 0 str ok */
@@ -26,13 +26,14 @@ char *str_concat(char *s1, char *s2)
 
 	if (s2)/* if size2 > 0 s2 ok */
 	{
-		i = 0;
-		while (s2[i] != '\0')
-			i++;
+		j = 0;
+		while (s2[j] != '\0')
+			j++;
 	}
-	size2 = i + 1;/* size s2, include '\0' */
+	size2 = j;/* size s2, include '\0' */
+	size3 = size1 + size2;
 
-	strconcat = malloc((size1 + size2) * sizeof(char));/*size2include'\0'*/
+	strconcat = malloc((size3 * sizeof(char)) + 1);/*size2include'\0'*/
 	if (!strconcat) /* if strconcat NULL*/
 		return (NULL);
 	for (i = 0; i < size1; i++)/* reinicialized i*/
@@ -40,7 +41,6 @@ char *str_concat(char *s1, char *s2)
 	for (j = 0; j < size2; j++, i++)/* j init in size1 */
 		strconcat[i] = s2[j];
 
-	/*size3 = size1 + size2;*/
-	strconcat[i] = '\0';
+	strconcat[size3 + 1] = '\0';
 	return (strconcat);
 }
