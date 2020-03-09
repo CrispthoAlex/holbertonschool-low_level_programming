f3dd50441c3397b1df573aacc986672fb60b16c1
crispthoalex <alex877@gmail.com>
2020-03-09 17:01:28 +0000

    computes the absolute value of a number

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,3 @@
-#ifndef FUNCTION_LIKE_MACROPI_H
-#define FUNCTION_LIKE_MACROPI_H
-
-#define ABS(X) ((X) < (0)? (-X) : (X))
+#define ABS(X) ((X) < (0)? -(X) : (X))
 
 #endif /* #ifndef _FILE_NAME_H_ */
