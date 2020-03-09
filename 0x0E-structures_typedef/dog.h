b2e55bea7578e2aa2ed8fcccb7054e0170844b6e
crispthoalex <alex877@gmail.com>
2020-03-09 23:20:53 +0000

    Define a new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Define a new type struct dog
+* @name: name dog
+* @age: age dog
+* @owner: owner dog
+ *
+* Description: Longer description
+ */
+
 struct dog
 {
 	char *name;
