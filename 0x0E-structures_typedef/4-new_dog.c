33ffb3c0d5e912df1f344a3ea034720ce5756b44
crispthoalex <alex877@gmail.com>
2020-03-10 04:44:19 +0000

    function that creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * *new_dog - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	(void *)name;
+	(void *)owner;
+}
