1b5d24018d9466cdbe41350e991ca16e128061b8
CrispthoAlex <alex877@gmail.com>
2020-03-10 16:57:39 -0500

    creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,15 +1,44 @@
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
 
 /**
- * *new_dog - check the code for Holberton School students.
+ *new_dog - define a new type
  * @name: dog name
- * @age: age dog
+ * @age: dog age
  * @owner: dog owner
- * Return: new_dog.
+ *Return: new_dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	(void *)name;
-	(void *)owner;
+	dog_t *dogger;
+	int len1 = 0, len2 = 0;
+	char *newname, *newowner;
+
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+	newname = malloc(len1 + 1);
+	if (!newname)
+	{
+		return (NULL);
+	}
+	newowner = malloc(len2 + 1);
+	if (!newowner)
+	{
+		free(newname);
+		return (NULL);
+	}
+	dogger = malloc(sizeof(dog_t));
+
+	if (!dogger)
+	{
+		free(newname);
+		free(newowner);
+		return (NULL);
+	}
+	_strcpy(newname, name);
+	_strcpy(newowner, owner);
+	dogger->name = newname;
+	dogger->age = age;
+	dogger->owner = newowner;
+	return o);
 }
