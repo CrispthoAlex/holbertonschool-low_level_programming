21d14085c603fbfdadb94990f2fdc5dbf6cd9c00
crispthoalex <alex877@gmail.com>
2020-03-11 22:31:24 +0000

    searches for an integer/index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,14 +11,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;/* same data type from size */
 
-	if (size <= 0)
-		return (-1);
-
 	if (cmp  && array)/* check if *array && *action exist */
 	{
+		if (size <= 0)
+			return (-1);
 		for (i = 0; i < size; i++)
 		{
-			if (cmp(array[i]) == 1) /* pointer to each function */
+			if (cmp(array[i]) != 0)
+/* return diff 0 pointertoeachelement  */
 				return (i);
 		}
 		if (i == size)
