ee4cb41fd3206e4fbc4a9e3ef9474ea38bb1cd45
crispthoalex <alex877@gmail.com>
2020-03-11 20:00:23 +0000

    prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,8 +6,8 @@
  * @f: function pointer to which one
  * Return: Nothing.
  */
- void print_name(char *name, void (*f)(char *))
- {
-	 if (f)
-		 f(name);
- }
+void print_name(char *name, void (*f)(char *))
+{
+	if (f)
+		f(name);
+}
