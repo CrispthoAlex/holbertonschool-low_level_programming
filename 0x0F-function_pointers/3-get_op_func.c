9f49bc90968b8a8abab734e77e4fe3b03008f116
crispthoalex <alex877@gmail.com>
2020-03-12 02:12:19 +0000

    performs simple operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "3-calc.h"
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+* get_op_func - selects the correct function to operation asked
+* @s: string operator
+*
+*
+* Return: a integer for erro or result of operation
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+
+	while (i < 5)/* function use by user */
+	{
+		if (*s == *ops[i].op && s[1] == 0)
+		{
+/* s[1]=0 no more operators like arguments*/
+			return (*ops[i].f);
+		}
+		i++;
+	}
+	printf("Error");
+	exit(99);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "function_pointers.h"
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main - for performs simple operations
+ * @argc: count the arguments
+ * @argv: each argument in position i
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int n1;
+	int n2;
+
+	if (argc != 4)/* only takes argv[1] && argv[3] */
+	{
+		printf("Error");
+		return (98);
+	}
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
+	printf("%d\n", get_op_func(argv[2])(n1, n2));
+	return (0);
+}
