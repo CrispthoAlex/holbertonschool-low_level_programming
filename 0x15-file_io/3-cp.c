562960b0ec8820c4a0f4f7e7ffcdc7fa807e6805
crispthoalex <alex877@gmail.com>
2020-04-01 07:56:42 +0000

    fix requeriments

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,47 +7,39 @@
  * Return: 0
  */
 int main(int ac, char *av[])
-{
-/*file descriptors: fopen (file1) & f2open (file2), */
-/*letters it could fread:read from file1 and plett:print to file2*/
+{/*file descriptors: fopen (file1) & f2open (file2), letters it could */
+	/*fread:read from file1 and plett:print to file2*/
 int fopen, f2open, fread = 0, plett  = 0;
 char stroutin[1024];/*string/buffer to copy from file 1*/
 
 if (ac == 3)/*will content two filenames*/
-{
-	/* open file from copy*/
+{	/* open file from copy*/
 	fopen = open(av[1], O_RDONLY);
 	if (fopen == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-
 	/* open or create file2*/
 	f2open = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	/* 0664 = (S_IRUSR  00400 + S_IWUSR  00200 */
-	/* + S_IRGRP  00040 + S_IWGRP  00020 + S_IROTH  00004) */
+	/* 0664 = (S_IRUSR  00400 + S_IWUSR  00200 + S_IRGRP  00040 */
+	/*+ S_IWGRP  00020 + S_IROTH  00004) */
 	if (f2open == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
-
-	/* ## write content to file */
-	/* read bytes from file_from*/
+	/* ## write content to file : read bytes from file_from*/
 	fread = 1024;
 	while (fread == 1024)
 	{
 		fread = read(fopen, stroutin, 1024);
-		/* write content to file */
-		plett = write(f2open, stroutin, fread);
+		plett = write(f2open, stroutin, fread);/* write content to file */
 	}
 	/*if copy file 1 to file 2 fails, exit*/
 	if (fread == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	if (plett == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
-
 	/*if can not close file descriptor*/
 	if (close(fopen) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fopen), exit(100);
 	if (close(f2open) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f2open), exit(100);
-
 	return (0);
 }
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
