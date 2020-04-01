72cbee4ca870ec7d59c5144c8c03f5a83e3b8bbd
crispthoalex <alex877@gmail.com>
2020-04-01 02:03:54 +0000

    reads a text file and prints it to standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,14 +9,14 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	/* file descriptor, letters it could rlett:read and plett:print*/
-	size_t fdescrip, rlett = 0, plett  = 0;
+	ssize_t fdescrip, rlett = 0, plett  = 0;
 	char *str = NULL;
 
 	if (filename)
 	{
 		/* open file */
 		fdescrip = open(filename, O_RDONLY);
-		if(fdescrip == -1)
+		if (fdescrip == -1)
 			return (0);
 		str = malloc(sizeof(char) * letters);/*reserve memory space */
 		if (!str)
@@ -26,7 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		rlett = read(fdescrip, str, letters);
 		/* write content of file */
 		plett = write(STDOUT_FILENO, str, rlett);
-		if (rlett == -1 || plett == -1 )
+		if (rlett == -1 || plett == -1)
 			return (0);
 
 		free(str);
