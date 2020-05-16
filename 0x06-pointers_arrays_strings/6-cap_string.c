d025800defc20381afd1d4e056577334df5365fd
crispthoalex <alex877@gmail.com>
2020-05-16 06:24:51 +0000

    fix capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,13 +14,13 @@ char *cap_string(char *str)
 	if (str[0] >= 'a' && str[0] <= 'z')
 		str[0] -= 32; /* difference between a uppercase*/
 
-	for (; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (; j <= 12; j++)
+		for (j = 0; j <= 12; j++)
 		{
 			if (separ[j] == str[i])
 			{
-				if (str[i + 1] >= 'a' || str[i + 1] <= 'z')
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 					str[i + 1] = str[i + 1] - 32;
 			}
 		}
