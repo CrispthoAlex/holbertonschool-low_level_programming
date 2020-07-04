77452e1cbe01d02c1833d999b19bcb6c189080b5
crispthoalex <alex877@gmail.com>
2020-07-04 02:54:21 +0000

    My island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def island_perimeter(grid):
+    """
+    A function that returns the perimeter of the island described in grid
+    """
+    if type(grid) is list:
+        perimeter = 0
+        for row in range(len(grid)):
+            for col in range(len(grid[row])):
+               if grid[row][col] == 1:
+                   rig = grid[row][col + 1] if col + 1 < len(grid[row]) else 0
+                   lef = grid[row][col - 1] if col - 1 >= 0 else 0
+                   up = grid[row - 1][col] if row - 1 >= 0 else 0
+                   down = grid[row + 1][col] if row + 1 < len(grid[row]) else 0
+                   perimeter += 4 * grid[row][col] - (rig + lef + up + down)
+        return perimeter
