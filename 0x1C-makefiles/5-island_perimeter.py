503187f4a3ab4ffe0b8593c06d55f58efdd0f0c4
crispthoalex <alex877@gmail.com>
2020-07-04 02:59:59 +0000

    My island: fix pep8

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+Create a function def island_perimeter(grid):
+that returns the perimeter of the island described in grid
+"""
+
 
 def island_perimeter(grid):
     """
@@ -8,10 +13,11 @@ def island_perimeter(grid):
         perimeter = 0
         for row in range(len(grid)):
             for col in range(len(grid[row])):
-               if grid[row][col] == 1:
-                   rig = grid[row][col + 1] if col + 1 < len(grid[row]) else 0
-                   lef = grid[row][col - 1] if col - 1 >= 0 else 0
-                   up = grid[row - 1][col] if row - 1 >= 0 else 0
-                   down = grid[row + 1][col] if row + 1 < len(grid[row]) else 0
-                   perimeter += 4 * grid[row][col] - (rig + lef + up + down)
+                if grid[row][col] == 1:
+                    rig = grid[row][col + 1] if col + 1 < len(grid[row]) else 0
+                    lef = grid[row][col - 1] if col - 1 >= 0 else 0
+                    up = grid[row - 1][col] if row - 1 >= 0 else 0
+                    down = grid[row + 1][col] if row + \
+                        1 < len(grid[row]) else 0
+                    perimeter += 4 * grid[row][col] - (rig + lef + up + down)
         return perimeter
