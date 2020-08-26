e42f380024d9fd992ef8acba3a4e255ca0049100
crispthoalex <alex877@gmail.com>
2020-08-26 00:35:24 +0000

    Add binary search algorithm file

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,56 @@
+#include "search_algos.h"
+
+/**
+* binary_search - function that searches for a value in an array of
+* integers using the Linear search algorithm
+* @array: *array is a pointer to the first element of the array
+* @size: is the number of elements in array
+* @value: is the value to search for
+* Return: index of value, else -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int ileft = 0, iright = (int)size - 1, mid = 0;
+
+	while (ileft <= iright)
+	{
+		print_array(array, ileft, iright);
+		/* Let's to search in 0 and final position */
+		if (array[ileft] == value)
+			return (ileft);
+		else if (array[iright] == value)
+			return (iright);
+		else /* Let's to search in middle position */
+		{
+			mid = (ileft + iright) / 2; /*  */
+			if (array[mid] == value)
+				return (mid);
+			else if (array[mid] < value)
+				ileft = mid + 1;
+			else  /* (array[mid] > value) */
+				iright = mid - 1;
+		}
+	}
+	return (-1);
+}
+
+/**
+ * print_array - print array given
+ * @array: *array is a pointer to the first element of the array
+ * @ileft: index left
+ * @iright: index right
+ * Return: void
+ */
+
+void print_array(int *array, int ileft, int iright)
+{
+	printf("Searching in array: ");
+	while (array && ileft <= iright)
+	{
+		printf("%d", array[ileft]);
+		if (ileft != iright)
+			printf(", ");
+		ileft++;
+	}
+	printf("\n");
+}
