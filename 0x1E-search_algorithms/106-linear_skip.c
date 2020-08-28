2bd1dfaf8b35e9ae9aa96f388641ee48300b2de9
crispthoalex <alex877@gmail.com>
2020-08-28 09:38:24 +0000

    Make a helper function

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -2,6 +2,8 @@
 #include "search_algos.h"
 #include <math.h>
 
+skiplist_t *linear_search_aux(skiplist_t *head, skiplist_t *list, int value);
+
 /**
 * linear_skip - function that searches for a value in a sorted skip list
 * of integers
@@ -11,32 +13,55 @@
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *head = list;
-	skiplist_t *temp = NULL;
+	skiplist_t *head = list; /* validate lower bound of block search */
+	skiplist_t *temp = head; /* validate upper bound of block search */
+	/* string to values check and index finded */
+	char *val_check = "Value checked at index [%lu] = [%d]\n";
+	char *val_index = "Value found between indexes [%lu] and [%lu]\n";
 
 	if (!head)
 		return (NULL);
 
-	while (head->express || head->next)
+	while (temp->express) /*|| head->next)*/
 	{
-		if (head->express)  /* Update temp  */
-			temp = head->express;
-		printf("Value checked at index [%lu] = [%d]\n", temp->index, temp->n);
-		if (temp->n < value && temp->express)
-			head = head->express;
-		else
-			break;
+		/*if (head->express)   Update temp  */
+		head = temp;
+		temp = temp->express;
+		printf(val_check, temp->index, temp->n);
+		if (temp->n >= value) /*&& temp->express)*/
+		{
+			printf(val_index, head->index, temp->index);
+			return (linear_search_aux(head, temp, value));
+		}
+		head = head->express;
 	}
+	head = temp; /* step back to last express, no value in linked list */
+	while (temp->next)
+		temp = temp->next;
+	printf(val_index, head->index, temp->index);
 
-	printf("Value found between indexes [%lu] and [%lu]\n",
-	       head->index, temp->index);
+	return (linear_search_aux(head, temp, value));
+}
 
-	while (head) /* && head->index <= step) */
+
+/**
+ * linear_search_aux - searches for a value in an array of integers using
+ * the Linear search algorithm
+ * @head: lower bound of block of search
+ * @temp: upper bound of block of search
+ * @value: value to search
+ *Return: index of value, else -1
+ */
+skiplist_t *linear_search_aux(skiplist_t *head, skiplist_t *temp, int value)
+{
+	char *val_check = "Value checked at index [%lu] = [%d]\n";
+
+	while (head && head->index <= temp->index)
 	{
-		printf("Value checked at index [%lu] = [%d]\n", head->index, head->n);
+		printf(val_check, head->index, head->n);
 		if (head->n == value)
 			return (head);
-		head = head->next; /* inprev++; */
+		head = head->next;
 	}
 	return (NULL);
 }
